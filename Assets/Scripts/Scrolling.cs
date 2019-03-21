6e2a6fbb0fbd2c5ff84202bae0297400a04b2fec
robbix1206 <robbix1206@gmail.com>
2019-03-21 23:09:47 +0100

    Move Scrolling into script

---
+++ Assets/Scripts/Scrolling.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrolling : MonoBehaviour
+{
+    public float speed;
+    public Sprite sprite;
+    public GameObject[] floor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (GameObject ground in floor)
+            ground.GetComponent<SpriteRenderer>().sprite = sprite;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (GameObject ground in floor)
+        {
+            if (ground.transform.position.x < -20)
+                ground.transform.position += new Vector3(40, 0, 0);
+            else
+                ground.transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+    }
+}

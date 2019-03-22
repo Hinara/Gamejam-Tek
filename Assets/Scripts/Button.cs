7629e81e19d9e895a26b1a9290c7c0dbbaa21308
Max <lol>
2019-03-22 14:23:24 +0100

    Add warning, menu, credits

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Button : MonoBehaviour
+{
+    public string nextScene;
+
+    // Start is called before the first frame update
+    private void OnMouseDown()
+    {
+        SceneManager.LoadScene(nextScene);
+    }
+}

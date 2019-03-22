9371f55688c8ec94a847989f5dd47f83a096b68d
Max <lol>
2019-03-22 14:37:07 +0100

    Add connection between scenes and Quit

---
+++ Assets/Scripts/QuitButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitButton : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    private void OnMouseDown()
+    {
+        Application.Quit();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+}
